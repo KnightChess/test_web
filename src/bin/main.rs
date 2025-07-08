a0d28dfabdab66bbfab7ef07d8b101d63b246ee7
KnightChess <wulingqi07@qq.com>
2025-07-08 22:26:57 +0800

    stop web service

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,6 +1,5 @@
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
-use std::thread::Thread;
 use std::time::Duration;
 use std::{fs, thread};
 use test_web::ThreadPool;
@@ -15,28 +14,78 @@ fn main() {
     }
 }
 
+#[derive(Debug)]
+enum Request {
+    Get,
+    Sleep,
+    Stop,
+}
+
+impl Request {
+    fn as_bytes(&self) -> &[u8] {
+        match self {
+            Request::Get =>  b"GET / HTTP/1.1\r\n",
+            Request::Sleep => b"GET /sleep HTTP/1.1\r\n",
+            Request::Stop => b"GET /stop HTTP/1.1\r\n"
+        }
+    }
+    
+    fn as_respond_str(&self) -> &str {
+        match self {
+            Request::Get | Request::Sleep=> "HTTP/1.1 200 OK",
+            Request::Stop => "HTTP/1.1 404 NOT FOUND"
+        }
+    }
+    
+    fn from_bytes(bytes: [u8; 1024]) -> Request {
+        if bytes.starts_with(Request::Get.as_bytes()) {
+            Request::Get
+        } else if bytes.starts_with(Request::Sleep.as_bytes()) {
+            thread::sleep(Duration::from_secs(5));
+            Request::Sleep
+        } else {
+            Request::Stop
+        }
+    }
+}
+
+#[derive(Debug)]
+enum Response {
+    Hello,
+    FourFour,
+}
+
+impl Response {
+    fn from_request(req: &Request) -> Response {
+        match req {
+            Request::Get => Response::Hello,
+            Request::Sleep => Response::Hello,
+            Request::Stop => Response::FourFour,
+        }
+    }
+    
+    fn get_resource(&self) -> &str {
+        match self {
+            Response::Hello => "hello.html",
+            Response::FourFour => "404.html",
+        }
+    }
+}
+
+
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
 
     stream.read(&mut buffer).unwrap();
 
-    let get = b"GET / HTTP/1.1\r\n";
-    let sleep = b"GET /sleep HTTP/1.1\r\n";
+    let req = Request::from_bytes(buffer);
+    let rep = Response::from_request(&req);
 
-    let (status_line, filename) = if buffer.starts_with(get) {
-        ("HTTP/1.1 200 OK", "hello.html")
-    } else if buffer.starts_with(sleep) {
-        thread::sleep(Duration::from_secs(5));
-        ("HTTP/1.1 200 OK", "hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
-    };
-
-    let contents = fs::read_to_string(filename).unwrap();
+    let contents = fs::read_to_string(rep.get_resource()).unwrap();
 
     let response = format!(
         "{}\r\nContent-Length: {}\r\n\r\n{}",
-        status_line,
+        req.as_respond_str(),
         contents.len(),
         contents
     );
--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ use std::thread;
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
-    sender: mpsc::Sender<Job>,
+    sender: mpsc::Sender<Message>,
 }
 
 type Job = Box<dyn FnOnce() -> () + Send + 'static>;
@@ -31,12 +31,18 @@ impl ThreadPool {
         F: FnOnce() + Send + 'static,
     {
         let job = Box::new(f);
-        self.sender.send(job).unwrap();
+        self.sender.send(Message::NewJob(job)).unwrap();
     }
 }
 
 impl Drop for ThreadPool {
     fn drop(&mut self) {
+        println!("Sending terminate message to all workers.");
+
+        for _ in &mut self.workers {
+            self.sender.send(Message::Terminate).unwrap();
+        }
+
         for worker in &mut self.workers {
             println!("Shutting down worker {}", worker.id);
             if let Some(thread) = worker.thread.take() {
@@ -52,17 +58,24 @@ struct Worker {
 }
 
 impl Worker {
-    fn new(id: usize, receiver: Arc<Mutex<Receiver<Job>>>) -> Worker {
+    fn new(id: usize, receiver: Arc<Mutex<Receiver<Message>>>) -> Worker {
         let thread = thread::spawn(move || {
             // while let Ok(job) = receiver.lock().unwrap().recv()
             // 上面代码无法达到预期行为，lock 会在整个 while 块中持有锁，执行 job() 也会持有锁
             // Mutex 结构体没有公有 unlock 方法，因为锁的所有权依赖 lock 方法返回的 LockResult<MutexGuard<T>> 中 MutexGuard<T> 的生命周期
             loop {
-                let job = receiver.lock().unwrap().recv().unwrap();
+                let message = receiver.lock().unwrap().recv().unwrap();
 
-                println!("Worker {} got a job; executing.", id);
-
-                job()
+                match message {
+                    Message::NewJob(job) => {
+                        println!("Worker {} got a job; executing.", id);
+                        job()
+                    }
+                    Message::Terminate => {
+                        println!("Worker {} was told to terminate.", id);
+                        break;
+                    }
+                }
             }
         });
 
@@ -71,4 +84,9 @@ impl Worker {
             thread: Some(thread),
         }
     }
+}
+
+enum Message {
+    NewJob(Job),
+    Terminate,
 }
